c174d203a49d4f5e1901ddd2bf3d8bad1871bc3d
Wanderson.jpeg <81868579+WandersonSantoos@users.noreply.github.com>
2023-06-11 20:55:35 -0300

    Foram feitas algumas melhorias
    Ainda existe um pequeno erro em alguns elementos da nav.

--- assets/js/app.js
+++ assets/js/app.js
@@ -116,22 +116,21 @@ let swiper = new Swiper('.portfolio-container', {
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
-    const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
+        const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
         }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
-}
-window.addEventListener('scroll', scrollActive)
-
+};
+window.addEventListener('scroll', scrollActive);
 
 /*========== Mudar o fundo do cabeçalho ==========*/
 function scrollHeader(){
@@ -241,7 +240,7 @@ class FormSubmit {
   const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
-    success: "<h1 class='success'>Sua mensagem foi enviada!</h1>",
+    success: "<h1 class='success'>Sua mensagem foi enviada!<br>Logo entraremos em contato.</h1>",
     error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
   });
   formSubmit.init();
